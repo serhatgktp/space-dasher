3315575c95a1a0add6d71044c1885088c8366c04
serhatgktp <serhatgktp@hotmail.com>
2022-04-03 13:11:15 -0400

    Added character drawing

--- game.asm
+++ game.asm
@@ -37,18 +37,41 @@
 .eqv BASE_ADDRESS 0x10008000
 .text
 li $t0, BASE_ADDRESS # $t0 stores the base address for display
-li $t1, 0xff0000 # $t1 stores the red colour code
-li $t2, 0x00ff00 # $t2 stores the green colour code
-li $t3, 0x0000ff # $t3 stores the blue colour code
-sw $t1, 0($t0) # paint the first (top-left) unit red.
-sw $t2, 4($t0) # paint the second unit on the first row green. Why $t0+4?
-sw $t3, 256($t0) # paint the first unit on the second row blue. Why +256?
+
+####################
+## DRAW CHARACTER ##
+####################
+li $t1, 0xad5745 # $t1 stores the blue colour code
+li $t2, 0xad9296 # gray-pink
+
+
+
+sw $t2, 0($t0)
+sw $t1, 4($t0)
+sw $t1, 8($t0)
+sw $t1, 12($t0)
+sw $t2, 16($t0)
+sw $t1, 256($t0) # paint the first unit on the second row blue. Why +256?
+sw $t1, 272($t0)
+sw $t2, 512($t0)
+sw $t1, 516($t0)
+sw $t1, 520($t0)
+sw $t1, 524($t0)
+sw $t2, 528($t0)
+sw $t2, 776($t0)
+sw $t2, 1028($t0)
+sw $t2, 1036($t0)
+sw $t2, 1028($t0)
+sw $t2, 1280($t0)
+sw $t2, 1296($t0)
+
 li $v0, 10 # terminate the program gracefully
 syscall
 
-li $t9, 0xffff0000
-lw $t8, 0($t9)
-beq $t8, 1, keypress_happened
+#li $t9, 0xffff0000
+#lw $t8, 0($t9)
+#beq $t8, 1, keypress_happened
 
-lw $t2, 4($t9) # this assumes $t9 is set to 0xfff0000 from before
-beq $t2, 0x61, respond_to_a # ASCII code of 'a' is 0x61 or 97 in decimal
+#lw $t2, 4($t9) # this assumes $t9 is set to 0xfff0000 from before
+#beq $t2, 0x61, respond_to_a # ASCII code of 'a' is 0x61 or 97 in decimal
+
